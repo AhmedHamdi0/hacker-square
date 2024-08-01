61f5ac7473848dfeace92f3a1144f46ddae9a063
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2024-08-01 06:27:34 +0300

    implement Datastore to extends all DAOs

---
+++ server/datastore/Datastore.ts
@@ -0,0 +1,7 @@
+import {UserDao} from "./UserDao";
+import {PostDao} from "./PostDao";
+import {LikeDao} from "./LikeDao";
+import {CommentDao} from "./CommentDao";
+import {InMemoryDatastore} from "./memoryDB/InMemoryDatastore";
+
+export interface Datastore extends UserDao, PostDao, LikeDao, CommentDao {}
