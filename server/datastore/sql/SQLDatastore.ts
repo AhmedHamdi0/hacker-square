75521e5d283952cee4530646a0e1d1b4c32277a3
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2024-08-02 19:38:11 +0300

    implement SQL datastore using sqlite

---
+++ server/datastore/sql/SQLDatastore.ts
@@ -0,0 +1,70 @@
+import { Datastore } from '../Datastore';
+import { Comment, Like, Post, User } from '../../types';
+import sqlite3 from 'sqlite3';
+import { Database, open } from 'sqlite';
+import path from 'path';
+
+
+export class SQLDatastore implements Datastore {
+    private db!: Database<sqlite3.Database,sqlite3.Statement>;
+    public async openDb() {
+        this.db = await open({
+            filename: path.join(__dirname, 'hacker-square.sqlite'),
+            driver: sqlite3.Database
+        })
+
+        this.db.run('PRAGMA foreign_keys = ON;')
+
+        await this.db.migrate({
+            migrationsPath: path.join(__dirname, 'migrations'),
+        })
+
+        return this;
+    }
+
+    async createPost(post: Post): Promise<void> {
+            await this.db.run('INSERT INTO posts (id, title, url, userId, postedAt) VALUES (?,?,?,?,?)',
+            post.id, post.title, post.url, post.userId, post.postedAt)
+    }
+
+    getPost(id: string): Promise<Post | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    deletePost(id: string): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+
+    listPosts(): Promise<Post[]> {
+        return this.db.all<Post[]>('SELECT * FROM posts');
+    }
+
+    async createUser(user: User): Promise<void> {
+        await this.db.run('INSERT INTO users (id, firstName, lastName, userName, email, password) VALUES (?,?,?,?,?,?)',
+            user.id, user.firstName, user.lastName, user.username, user.email, user.password)
+    }
+
+    getUserByEmail(email: string): Promise<User | undefined> {
+        return this.db.get<User>(`SELECT * FROM users WHERE email = ?`, email)
+    }
+
+    getUserByUsername(username: string): Promise<User | undefined> {
+        return this.db.get<User>(`SELECT * FROM users WHERE username = ?`, username)
+    }
+
+    createComment(comment: Comment): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+
+    deleteComment(id: string): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+
+    listComments(postId: string): Promise<Comment[]> {
+        return Promise.resolve([]);
+    }
+
+    createLike(like: Like): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+}
