3cb88b06d6e397036f4b1a10ebb5e035da232bb4
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2024-08-02 19:40:11 +0300

    add user end points for signUp and signIn

--- server/datastore/Datastore.ts
+++ server/datastore/Datastore.ts
@@ -3,7 +3,13 @@ import {PostDao} from "./dao/PostDao";
 import {LikeDao} from "./dao/LikeDao";
 import {CommentDao} from "./dao/CommentDao";
 import {InMemoryDatastore} from "./memoryDB/InMemoryDatastore";
+import { SQLDatastore } from './sql/SQLDatastore';
 
 export interface Datastore extends UserDao, PostDao, LikeDao, CommentDao {}
 
-export const db = new InMemoryDatastore();
+export let db: Datastore;
+
+export async function initDb() {
+    // db = new InMemoryDatastore();
+    db = await new SQLDatastore().openDb();
+}
--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,31 @@
 import express, { ErrorRequestHandler, RequestHandler } from 'express';
 import { createPostHandler, listPostsHandler } from './handlers/postHandler';
 import asyncHandler from 'express-async-handler';
+import { initDb } from './datastore/Datastore';
+import { signInHandler, signUpHandler } from './handlers/userHandler';
 
-const app = express();
-app.use(express.json());
+(async () => {
+    await initDb();
+    const app = express();
+    app.use(express.json());
 
-const requestLoggerMiddleware: RequestHandler = (req, _, next) => {
-    console.log(req.method, req.path, req.body);
-    next();
-}
-const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
-    console.log('Error:', error);
-    return response.status(500).send('Oops, an unexpected error occurred, please try again!!');
-}
+    const requestLoggerMiddleware: RequestHandler = (req, _, next) => {
+        console.log(req.method, req.path, req.body);
+        next();
+    }
 
-app.use(errorHandler);
-app.use(requestLoggerMiddleware);
+    const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+        console.log('Error:', error);
+        return response.status(500).send('Oops, an unexpected error occurred, please try again!!');
+    }
 
-app.get('/posts', asyncHandler(listPostsHandler));
-app.post('/posts', asyncHandler(createPostHandler));
+    app.get('/posts', asyncHandler(listPostsHandler));
+    app.post('/posts', asyncHandler(createPostHandler));
+    app.post('/signup', asyncHandler(signUpHandler))
+    app.post('/signin', asyncHandler(signInHandler))
 
-app.listen(3000);
+    app.use(errorHandler);
+    app.use(requestLoggerMiddleware);
+
+    app.listen(8000);
+})();
