ec5ddd2ba6812c657c07fb9af6659d6ea532d5bb
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2024-09-16 00:02:33 +0300

    move server files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-import { createPostHandler, listPostsHandler } from './handlers/postHandler';
+import { createPostHandler, listPostsHandler } from '../handlers/postHandler';
 import asyncHandler from 'express-async-handler';
-import { initDb } from './datastore/Datastore';
-import { signInHandler, signUpHandler } from './handlers/authHandler';
-import { errorHandler } from './middleware/errorMiddleware';
-import { requestLoggerMiddleware } from './middleware/loggerMiddleware';
+import { initDb } from '../datastore/Datastore';
+import { signInHandler, signUpHandler } from '../handlers/authHandler';
+import { errorHandler } from '../middleware/errorMiddleware';
+import { requestLoggerMiddleware } from '../middleware/loggerMiddleware';
 import dotenv from 'dotenv';
-import { authMiddleware } from './middleware/authMiddleware';
+import { authMiddleware } from '../middleware/authMiddleware';
+import cors from 'cors';
 
 
 (async () => {
@@ -15,6 +16,7 @@ import { authMiddleware } from './middleware/authMiddleware';
 
     const app = express();
     app.use(express.json());
+    app.use(cors());
 
     app.use(requestLoggerMiddleware);
 
@@ -22,12 +24,11 @@ import { authMiddleware } from './middleware/authMiddleware';
     app.post('/signup', asyncHandler(signUpHandler));
     app.post('/signin', asyncHandler(signInHandler));
 
-    app.use(authMiddleware);
+    // app.use(authMiddleware);
 
     app.get('/posts', asyncHandler(listPostsHandler));
     app.post('/posts', asyncHandler(createPostHandler));
 
     app.use(errorHandler);
-
     app.listen(8000);
 })();
